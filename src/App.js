4ae6bc93e424c7d1225b86ba82c1884d2183bca6
Harshit chopde <109473586+Harshitchopde@users.noreply.github.com>
2023-12-20 18:59:51 +0530

    create-react-app for tailwind

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
-      App
+     tailwind app
+     <div className="bg-black w-11/12 h-[300px]"></div>
+
      
     </div>
   );
