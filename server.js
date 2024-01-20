222dd9c6ff316da870469f23b53678906161a1d4
Harshit chopde <109473586+Harshitchopde@users.noreply.github.com>
2024-01-20 19:04:43 +0530

    Updated At 20012024

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 import express from "express";
-import 'dotenv/config'
-// const dotenv = require("dotenv")
+import 'dotenv/config' //  to load the data to `process` variable
 import { Connect } from "./config/connection.js";
 import demoRoutes from "./routes/demoRoutes.js"
 
 const app = express();
 
-// app.use(dotenv.config())// to load the data to `process` variable
+
 app.use(express.json())// parser to use json
 const PORT = process.env.PORT || 4000
 app.get("/", (req, res) => {
@@ -14,13 +13,13 @@ app.get("/", (req, res) => {
     res.send("Hello world")
 
 })
-// console.log(dotenv.config());
+
 app.use('/api/demo',demoRoutes)
 // default path
 app.use('/',(req,res)=>{
     res.send("<H1> NAMASTE </H1>")
 })
-console.log(process.env.MONGODB);
+
 
 app.listen(PORT, () => {
     Connect()
